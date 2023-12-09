e985581b22b2a574d22e34219ca23654e0ccf9c6
Pragya Sharma <emailpragyasharma@gmail.com>
2023-12-08 20:51:49 -0800

    fixing rating math!

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class Classroom(db.Model):
    id=db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
    class_name = db.Column(db.String(100), nullable=False)
    description = db.Column(db.String(1000), unique=True, nullable=False)
-   rating = db.Column(db.Integer)
+   sumOfRating = db.Column(db.Integer)
    numOfRatings = db.Column(db.Integer)
 
    # teacher name 
@@ -46,16 +46,20 @@ def add_rating():
 
    if classroom == None:
        return f"None such class '{class_id}'", 400
+   
+   # number of people who have rated 
    num_current_ratings = classroom.numOfRatings
+   # sum of total ratings 
+   num_total_ratings = classroom.sumOfRatings + rating
+    
+
+   classroom.numOfRatings +=1 ## increment total first so rating is accurate 
    
-   # have some variable new_rating that contains the updated rating
-   # update classroom.rating = new_rating
-   # increment numOfRatings
-   
-   classroom.rating = (rating + classroom.rating) / num_current_ratings
-   classroom.numOfRatings += 1;
+   # the real rating to be displayed to user!!
+   actualRating = (num_total_ratings) / num_current_ratings
    
    db.session.add(classroom)
    db.session.commit()
+   
    return "done", 200
 
